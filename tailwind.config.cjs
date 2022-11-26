e30f2b5d709aafd86cfa4c8e36b877432447f868
Ben Borgers <borgersbenjamin@gmail.com>
2022-11-26 18:28:07 -0500

    Start building

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,11 +4,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Nunito", ...defaultTheme.fontFamily.sans],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
